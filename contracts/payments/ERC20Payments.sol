4859881c8b4725ca885030fe79d74de3381cc8ed
Luke Hannigan <lukeyhannigany@gmail.com>
2022-06-22 01:18:08 +0100

    erc20payements

--- contracts/payments/ERC20Payments.sol
+++ contracts/payments/ERC20Payments.sol
@@ -2,28 +2,22 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-interface IERC20Payments {
+
+contract ERC20Payments {
+    using SafeERC20 for IERC20;
     struct Payee {
         address addr;
         uint weighting;
     }
-    event Initialised(IERC20 token);
-    event Payment(address to, uint payment);
     event PayeeAdded(Payee payee);
     event PayeesDeleted();
-    function addPayee(Payee calldata payee) external;
-    function deletePayees() external;
-}
-contract ERC20Payments is IERC20Payments, Ownable {
-    using SafeERC20 for IERC20;
     Payee[] private _payees;
     uint private _totalWeighting;
     IERC20 private _token;
     constructor(IERC20 token) {
         _token = token;
-        emit Initialised(token);
     }
-    function addPayee(Payee calldata payee) external onlyOwner {
+    function _addPayee(Payee calldata payee) internal {
         require(payee.weighting > 0, "All payees must have a weighting.");
         require(payee.addr != address(0), "Payee cannot be the zero address.");
         _totalWeighting += payee.weighting;
@@ -36,10 +30,9 @@ contract ERC20Payments is IERC20Payments, Ownable {
             Payee storage payee = _payees[i];
             uint payment = (payee.weighting * value) / _totalWeighting;
             _token.safeTransfer(payee.addr, payment);
-            emit Payment(payee.addr, payment);
         }
     }
-    function deletePayees() external override onlyOwner {
+    function _deletePayees() internal {
         delete _totalWeighting;
         delete _payees;
         emit PayeesDeleted();
