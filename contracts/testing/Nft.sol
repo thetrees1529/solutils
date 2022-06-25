c9a322d242424e1fede58e95f159b673fed70931
Luke Hannigan <lukeyhannigany@gmail.com>
2022-06-25 19:02:23 +0100

    strings

--- contracts/testing/Nft.sol
+++ contracts/testing/Nft.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract Nft is ERC721("Nft","NFT"), Ownable {
+contract Nft is ERC721Enumerable, Ownable {
+    constructor( )ERC721("Nft","NFT"){}
     function mint(address account, uint tokenId) external onlyOwner {
         _mint(account, tokenId);
     }
