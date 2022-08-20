18383aee27cb1a5eaa52adec1a184ca2fc2a629d
Luke Hannigan <lukeyhannigany@gmail.com>
2022-08-20 20:19:08 +0100

    payments

--- contracts/payments/Payments.sol
+++ contracts/payments/Payments.sol
@@ -1,10 +1,8 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract ERC20Payments {
-    using SafeERC20 for IERC20;
     struct Payee {
         address addr;
         uint weighting;
@@ -13,10 +11,6 @@ contract ERC20Payments {
     event PayeesDeleted();
     Payee[] private _payees;
     uint private _totalWeighting;
-    IERC20 private _token;
-    constructor(IERC20 token) {
-        _token = token;
-    }
     function getPayees() external view returns(Payee[] memory) {
         return _payees;
     }
@@ -35,7 +29,8 @@ contract ERC20Payments {
         for(uint i; i < _payees.length; i ++) {
             Payee storage payee = _payees[i];
             uint payment = (payee.weighting * value) / _totalWeighting;
-            _token.safeTransfer(payee.addr, payment);
+            (bool succ,) = payee.addr.call{value: payment}("");
+            require(succ, "Issue with one of the payees.");
         }
     }
     function _makePayment(uint value) internal {
