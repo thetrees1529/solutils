f097e3f723cb67c2b2bd983c211a6e8ad9b507be
Luke Hannigan <lukeyhannigany@gmail.com>
2022-09-15 23:41:38 +0100

    ownerOd

--- contracts/gamefi/OwnerOf.sol
+++ contracts/gamefi/OwnerOf.sol
@@ -1,12 +1,21 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-library OwnerOf {
-    function _isOwnerOf(IERC721 token, uint tokenId,address account) internal view returns(bool) {
-        return token.ownerOf(tokenId) == account;
+contract OwnerOf {
+
+    IERC721 private _token;
+
+    constructor(IERC721 token) {
+        _token = token;
     }
-    modifier onlyOwnerOf(IERC721 token, uint tokenId, address account) {
-        require(_isOwnerOf(token, tokenId,account), "Does not own nft.");
+    
+    function isOwnerOf(address account, uint tokenId) private view returns(bool) {
+        return _token.ownerOf(tokenId) == account;
+    }
+
+    modifier onlyOwnerOf(uint tokenId) {
+        require(isOwnerOf(msg.sender, tokenId), "Does not own nft.");
         _;
     }
+
 }
