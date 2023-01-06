e65978ff848a415288312f610139638c75bb6bcd
unknown <lukeyhannigany@gmail.com>
2023-01-06 19:11:31 +0000

    .

--- contracts/gamefi/Nft.sol
+++ contracts/gamefi/Nft.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract Nft is AccessControl, ERC721Enumerable {
+abstract contract Nft is AccessControl, ERC721Enumerable {
 
     string private baseUri;
 
-    constructor(string memory name, string memory symbol, string memory uri) ERC721(name, symbol) {
+    constructor(string memory uri) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setBaseURI(uri);
     }
