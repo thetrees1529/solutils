617ba10e279b87799e9e22ee9a83ce95bccf3243
Luke Hannigan <lukeyhannigany@gmail.com>
2023-05-09 05:57:57 +0100

    account first

--- contracts/gamefi/OwnerOf.sol
+++ contracts/gamefi/OwnerOf.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 library OwnerOf {
     
-    function isOwnerOf(IERC721 token, address account, uint tokenId) internal view returns(bool) {
+    function isOwnerOf(address account, IERC721 token, uint tokenId) internal view returns(bool) {
         return token.ownerOf(tokenId) == account;
     }
 
