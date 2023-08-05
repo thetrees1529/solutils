8fc970e03ac4b654cace01b1ccb6fe36bea8a966
thetrees1529 <54856258+thetrees1529@users.noreply.github.com>
2023-08-05 22:09:14 +0000

    burn

--- contracts/gamefi/Nft.sol
+++ contracts/gamefi/Nft.sol
@@ -8,9 +8,13 @@ abstract contract Nft is AccessControl, ERC721Enumerable {
 
     string private baseUri;
     uint private _nextTokenId;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
     constructor(string memory uri) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(BURNER_ROLE, msg.sender);
         _setBaseURI(uri);
     }
 
@@ -18,7 +22,7 @@ abstract contract Nft is AccessControl, ERC721Enumerable {
         return super.supportsInterface(interfaceId);
     }
 
-    function mint(address to, uint numberOf) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function mint(address to, uint numberOf) external onlyRole(MINTER_ROLE) {
         uint tokenId = _nextTokenId;
         _nextTokenId += numberOf;
         for(uint i; i < numberOf; i ++){
@@ -27,7 +31,7 @@ abstract contract Nft is AccessControl, ERC721Enumerable {
         }
     }
 
-    function burn(uint[] calldata tokenIds) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function burn(uint[] calldata tokenIds) external onlyRole(BURNER_ROLE) {
         for(uint i; i < tokenIds.length; i ++) {
             _burn(tokenIds[i]);
         }
