eb0bf101e77fa3c447dda4e721e758f1902645ce
Luke Hannigan <lukeyhannigany@gmail.com>
2024-11-09 13:36:17 +0000

    payee store

---
+++ contracts/payments/ERC20PayeeStore.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: Unlicenced
+pragma solidity ^0.8.0;
+import "./ERC20Payments.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC20PayeeStore is Ownable {
+    ERC20Payments.Payee[] private _payees;
+
+    function getPayees() public view returns (ERC20Payments.Payee[] memory) {
+        return _payees;
+    }
+
+    function setPayees(ERC20Payments.Payee[] calldata payees) public onlyOwner {
+        delete _payees;
+        for(uint i; i < payees.length; i ++) {
+            _payees.push(payees[i]);
+        }
+    }
+}
