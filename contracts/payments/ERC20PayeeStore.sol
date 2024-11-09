d147d826d3b07e9b2559f9d228ebdd9bf12e987b
Luke Hannigan <lukeyhannigany@gmail.com>
2024-11-09 14:10:48 +0000

    payee store

--- contracts/payments/ERC20PayeeStore.sol
+++ contracts/payments/ERC20PayeeStore.sol
@@ -6,11 +6,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract ERC20PayeeStore is Ownable {
     ERC20Payments.Payee[] private _payees;
 
+    constructor(ERC20Payments.Payee[] memory payees) {
+        _setPayees(payees);
+    }
+
     function getPayees() public view returns (ERC20Payments.Payee[] memory) {
         return _payees;
     }
 
     function setPayees(ERC20Payments.Payee[] calldata payees) public onlyOwner {
+        _setPayees(payees);
+    }
+
+    function _setPayees(ERC20Payments.Payee[] memory payees) internal {
         delete _payees;
         for(uint i; i < payees.length; i ++) {
             _payees.push(payees[i]);
