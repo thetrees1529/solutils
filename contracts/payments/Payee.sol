3620dbbb9b21d0494294ac95eccd4f0cfbea401c
Luke Hannigan <lukeyhannigany@gmail.com>
2024-12-04 19:27:34 +0000

    payee

--- contracts/payments/Payee.sol
+++ contracts/payments/Payee.sol
@@ -4,16 +4,15 @@ pragma solidity ^0.8.0;
 import "./IPayee.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 abstract contract Payee is ERC165, IPayee {
-    address public approvedSender;
-    constructor(address approvedSender_) {
-        approvedSender = approvedSender_;
+    mapping(address => bool) private _approvedSenders;
+    constructor() {
     }
     function supportsInterface(bytes4 interfaceId) public view virtual override(IERC165, ERC165) returns (bool) {
         return interfaceId == type(IPayee).interfaceId || super.supportsInterface(interfaceId);
     }
 
     function onPaymentReceived(IERC20 token, address from, uint256 value) external override {
-        require(msg.sender == approvedSender, "Payee: Only approved sender can call this function");
+        require(_approvedSenders[msg.sender], "Payee: Only approved sender can call this function");
         handlePaymentReceived(token, from, value);
     }
 
